a400a3774e07cb1317a56d52587a0181064ed899
xiekailian <460715662@qq.com>
2018-01-26 13:24:14 +0800

    登录界面框架搭建和跳转

--- app/src/main/java/com/reality/realityapp/LoginActivity.java
+++ app/src/main/java/com/reality/realityapp/LoginActivity.java
@@ -1,13 +1,62 @@
 package com.reality.realityapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText usernameEdt;
+    private EditText passwordEdt;
+    private Button loginBtn;
+    private TextView registerTv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        initView();
+        initEvent();
+
+    }
+
+    private void initView() {
+        usernameEdt = (EditText) findViewById(R.id.id_edt_username);
+        passwordEdt = (EditText) findViewById(R.id.id_edt_password);
+        loginBtn = (Button) findViewById(R.id.id_btn_login);
+        registerTv = (TextView) findViewById(R.id.id_tv_register);
+    }
+
+    private void initEvent() {
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO 检测登录是否成功
+                toFirstPageActivity();
+            }
+        });
+
+        registerTv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toRegisterActivity();
+            }
+        });
+    }
+
+    private void toFirstPageActivity() {
+        Intent intent = new Intent(this,FirstPageActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    private void toRegisterActivity() {
+        Intent intent = new Intent(this,RegisterActivity.class);
+        startActivity(intent);
     }
 }
