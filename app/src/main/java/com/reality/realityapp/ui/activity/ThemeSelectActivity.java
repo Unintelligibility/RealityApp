82183ebc2b05996b624ed480d0bc674b7daab95a
xiekailian <460715662@qq.com>
2018-03-06 14:39:51 +0800

    回复ThemeSelectActivity到debug前的状态

--- app/src/main/java/com/reality/realityapp/ui/activity/ThemeSelectActivity.java
+++ app/src/main/java/com/reality/realityapp/ui/activity/ThemeSelectActivity.java
@@ -10,6 +10,6 @@ public class ThemeSelectActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_theme_select);
+        setContentView(R.layout.activity_theme_select);
     }
 }
