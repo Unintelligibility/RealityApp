e4f3c1f5be93a7ee2dafc05430ab9c01839774d0
ObserverZQ <657282348@qq.com>
2018-03-23 19:28:18 +0800

    登录注册界面修改一下

--- app/src/main/java/com/reality/realityapp/ui/view/fragment/MyInfoFragment.java
+++ app/src/main/java/com/reality/realityapp/ui/view/fragment/MyInfoFragment.java
@@ -6,16 +6,20 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.reality.realityapp.R;
+import com.reality.realityapp.UserInfoHolder;
 
 /**
  * Created by 铠联 on 2018/3/8.
  */
 
 public class MyInfoFragment extends Fragment {
-
-
+    private TextView textinput_username;
+    String username = UserInfoHolder.getInstance().getUser().getUsername();
     public static MyInfoFragment newInstance(){
         MyInfoFragment myInfoFragment = new MyInfoFragment();
         return myInfoFragment;
@@ -26,10 +30,15 @@ public class MyInfoFragment extends Fragment {
         super.onCreate(savedInstanceState);
     }
 
+//    private void initView() {
+//        textinput_username = (TextView) findViewById(R.id.id_tv_register);
+//    }
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_myinfo, null);
+
+//        textinput_username = (TextView)v.findViewById(R.layout.);
         return v;
 //        return super.onCreateView(inflater, container, savedInstanceState);
     }
